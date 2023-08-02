c477e58720f38b62fa4a110a78cc964dd60ead8b
sartoriova <140718279+sartoriova@users.noreply.github.com>
2023-08-01 23:49:38 -0300

    Update Usuario.php

--- Usuario.php
+++ Usuario.php
@@ -43,8 +43,8 @@
 
         public static function cadastrar($nome, $email, $senha, $nomeUser, $imgAvatar){
             try{
-                $nomeUser = "LOVETHEMUSICOWNER"
-                $imgAvatar = "imagens/imgAvatarDefault.jpeg"
+                $nomeUser = "LOVETHEMUSICOWNER";
+                $imgAvatar = "imagens/imgAvatarDefault.jpeg";
                 $senhaCriptografada = password_hash($senha, PASSWORD_BCRYPT);
 
                 $conexao = Conexao::getConexao();
